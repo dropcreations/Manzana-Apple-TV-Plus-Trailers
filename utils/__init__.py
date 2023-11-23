8cea0e02a0b890d87400994e1b89ef15643bbead
dropcodes <97192664+dropcreations@users.noreply.github.com>
2023-11-24 02:51:12 +0530

    Updated.

--- utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from utils import logger
-from utils.sanitize import sanitize
--- utils/sanitize.py
+++
@@ -1,9 +0,0 @@
-import unicodedata
-
-def sanitize(f):
-    b = ['\\', '/', ':', '*', '?', '"', '<', '>', '|', '\0']
-    f = ''.join(c for c in f if c not in b)
-    f = ''.join(c for c in f if 31 < ord(c))
-    f = unicodedata.normalize("NFKD", f)
-    f = f.rstrip(". ")
-    return f.strip()
